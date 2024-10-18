69549d30571d4cc3079698f283bb0479859cb547
LyneQ <lyne.quequin@gmail.com>
2024-10-18 10:06:25 +0200

    feat(auth): added login(), remove authenticate()

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,51 @@
-import bcrypt from 'bcrypt';
-import UserClass from '../models/user.js';
-
+import bcrypt from "bcrypt";
+import UserClass from "../models/user.js";
 
 async function register(req, res) {
-    try {
-        console.log(req.body);
-        const {email, password} = req.body;
-        const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).send("Email and password are required");
 
-        const User = new UserClass();
-        await User.createUser(email, encryptedPassword);
+    const encryptedPassword = await bcrypt.hash(password, 1);
+    const User = new UserClass();
+    const userCreated = await User.createUser(email, encryptedPassword);
 
-    } catch (error) {
-        console.log(error);
-        res.send("error");
+    if (userCreated) {
+      res.status(200).send("User successfully created");
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(error.sqlMessage);
+  }
 }
 
-async function login (req, res) {
-    // TODO login
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).send("Email and password are required");
+
+
+    const User = new UserClass();
+    const user = await User.getUserByEmail(email);
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      res.status(200).send("User successfully authenticated");
+    } else {
+      res.status(401).send("Invalid credentials");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(error.sqlMessage);
+  }
 }
 
-async function middleware (req, res, next) {
-    // TODO middleware
+async function middleware(req, res, next) {
+  // TODO middleware
 }
-
-// TODO Login et middleware
 
 export default {
-    register,
-    login,
-    middleware
+  register,
+  login,
+  middleware,
 };
--- models/user.js
+++ models/user.js
@@ -9,18 +9,17 @@ export default class User {
             const results = await conn.query('INSERT INTO users (email, password) VALUES (?,?)',[email, password]);
             return results.insertId; 
         }finally{
-            if (conn) conn.release();
+            if (conn) await conn.release();
         }
     }
 
-    async authenticate(email, password){
-
+    async getUserByEmail(email){
         const conn = await pool.getConnection();
         try{
-            const rows = await conn.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password]);
+            const rows = await conn.query('SELECT * FROM users WHERE email = ?', [email]);
             return rows[0];
         }finally{
-            if (conn) conn.release();
+            if (conn) await conn.release();
         }
     }
 }
