533b6c4fd34f33bf73c04239fbc0bba4f0e13f86
LyneQ <lyne.quequin@gmail.com>
2024-10-21 15:53:13 +0200

    fix(models): - update from mariadb to mysql

--- models/message.js
+++ models/message.js
@@ -1,56 +1,58 @@
-import pool from "../config/db.js";
+import connexion from "../config/db.js";
 
 export default class Message {
 
+  connexion;
+
   async sendMessages(lobbyID, userID, message) {
-    const conn = await pool.getConnection();
     try {
-      const lobbyExist = await conn.query(
+      this.connexion = connexion;
+      const lobbyExist = await this.connexion.query(
         "SELECT * FROM lobbies WHERE id = ?",
         [lobbyID],
       );
 
       if ( lobbyExist.length === 0 ) return false;
 
-      const results = await conn.query(
+      const results = await this.connexion.query(
         "INSERT INTO messages (lobby_id, user_id, content) VALUES (?,?,?)",
         [lobbyID, userID, message],
       );
       return results.insertId.toString();
-    } finally {
-      if (conn) await conn.release();
+    } catch (error) {
+      console.error(error);
+      return false;
     }
   }
 
   async getMessages(lobbyID) {
-    const conn = await pool.getConnection();
     try {
-        return await conn.query("SELECT * FROM messages WHERE lobby_id = ?", [lobbyID]);
-
-    } finally {
-        if (conn) await conn.release();
+        return await connexion.query("SELECT * FROM messages WHERE lobby_id = ?", [lobbyID]);
+    } catch (error) {
+      console.error(error);
+      return false;
     }
   }
 
   async getMessageById(lobbyID, messageID) {
-    const conn = await pool.getConnection();
     try {
-      return await conn.query("SELECT * FROM messages WHERE lobby_id = ? AND id = ?", [lobbyID, messageID]);
-    } finally {
-      if (conn) await conn.release();
+      return await connexion.query("SELECT * FROM messages WHERE lobby_id = ? AND id = ?", [lobbyID, messageID]);
+    } catch (error) {
+      console.error(error);
+      return false;
     }
   }
 
   async deleteMessage(messageID) {
-    const conn = await pool.getConnection();
     try {
-      const messageExist = await conn.query("SELECT * FROM messages WHERE id = ?", [messageID]);
+      const messageExist = await connexion.query("SELECT * FROM messages WHERE id = ?", [messageID]);
 
       if( messageExist.length === 0 ) return false;
 
-      return !!conn.execute("DELETE FROM messages WHERE id = ?", [messageID]);
-    } finally {
-      if (conn) await conn.release();
+      return !!connexion.execute("DELETE FROM messages WHERE id = ?", [messageID]);
+    } catch (error) {
+      console.error(error);
+      return false;
     }
   }
 
