27ca443828f730dad20a86aaa1eb4a88da83ad34
LyneQ <lyne.quequin@gmail.com>
2024-10-21 15:53:53 +0200

    fix(models): - update from mariadb to mysql

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,41 +2,51 @@ import MessageClass from "../models/message.js";
 
 /**
  * GET /api/lobby/[lobby-id]                An array containing all the message from the lobby
+ *
  * GET /api/lobby/[lobby-id]/[message-id]   A single message object from the lobby
+ *
  * POST /api/lobby/[lobby-id]               Send a message to the lobby
+ *
  * PATCH /api/lobby/[message-id]            edit the message (message owner or admin)
  */
 
 async function getMessages(req, res) {
   const { lobbyId } = req.params;
 
-  if (!lobbyId) return res.status(400).send("Bad request - missing lobbyID or messageID");
+  if (!lobbyId)
+    return res.status(400).send("Bad request - missing lobbyID or messageID");
 
   const Message = new MessageClass();
+  const fetchedMessages = await Message.getMessages(lobbyId);
+  const messages = fetchedMessages[0];
 
-  res.status(200).send(await Message.getMessages(lobbyId));
+  if (!messages || messages.length === 0)
+    return res.status(404).send("Not found - messages not found");
+
+  res.status(200).send(messages);
 }
 
 async function getMessage(req, res) {
-    const { lobbyId, messageId } = req.params;
+  const { lobbyId, messageId } = req.params;
 
-    const Message = new MessageClass();
+  const Message = new MessageClass();
 
-    if (!lobbyId || !messageId) return res.status(400).send("Bad request - missing lobbyID or messageID");
+  if (!lobbyId || !messageId)
+    return res.status(400).send("Bad request - missing lobbyID or messageID");
 
-    const message = await Message.getMessageById(lobbyId, messageId);
+  const fetchedMessage = await Message.getMessageById(lobbyId, messageId);
+  const message = fetchedMessage[0];
 
-    if (!message) return res.status(404).send("Not found - message not found");
+  if (!message || message.length === 0) {
+    return res.status(404).send("Not found - message not found");
+  }
 
-    if (message.length === 0) return res.status(404).send("Not Found - message not found");
-
-    res.status(200).send(message);
-
+  res.status(200).send(message);
 }
 
 async function postMessages(req, res) {
   const { lobbyId } = req.params;
-  const { content, userId} = req.body;
+  const { content, userId } = req.body;
 
   if (!lobbyId)
     return res.status(400).send("Bad request - missing lobbyID or messageID");
@@ -44,20 +54,19 @@ async function postMessages(req, res) {
   const Message = new MessageClass();
   const message = await Message.sendMessages(lobbyId, userId, content);
 
-  if (!message) return res.status(404).send("Not found - lobbyId not found");
-
   res.status(201).send(message);
 }
 
 async function deleteMessage(req, res) {
-  const {messageId} = req.params;
+  const { messageId } = req.params;
 
-  if (!messageId) return res.status(400).send("Bad request - missing messageID");
+  if (!messageId)
+    return res.status(400).send("Bad request - missing messageID");
 
   const Message = new MessageClass();
   const message = await Message.deleteMessage(messageId);
 
-  if ( !message ) return res.status(404).send("Not found - message not found");
+  if (!message) return res.status(404).send("Not found - message not found");
 
   res.status(200).send(message);
 }
