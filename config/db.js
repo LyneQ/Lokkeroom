3bdb4b4c3cd31bb2bbac525a229afadde3a655e2
Pedrocito97 <pierre.willemart.110497@gmail.com>
2024-10-21 14:59:35 +0200

    changed mariaDb to mysql

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-import mariadb from 'mariadb';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mariadb.createPool({
+const connexion = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
-export default pool;
+
+export default connexion;
